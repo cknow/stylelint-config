08bdf1cce64ee565459ba1b73f29a0499f4be487
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2017-05-04 11:11:09 -0300

    Add rules

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+    extends: [
+        './rules/at-rule.js',
+        './rules/block.js',
+        './rules/color.js',
+        './rules/comment.js',
+        './rules/custom-media.js',
+        './rules/custom-property.js',
+        './rules/declaration.js',
+        './rules/declaration-block.js',
+        './rules/font-family.js',
+        './rules/font-weight.js',
+        './rules/function.js',
+        './rules/general.js',
+        './rules/keyframe-declaration.js',
+        './rules/length.js',
+        './rules/media-feature.js',
+        './rules/media-query-list.js',
+        './rules/number.js',
+        './rules/property.js',
+        './rules/root-rule.js',
+        './rules/rule.js',
+        './rules/selector.js',
+        './rules/selector-list.js',
+        './rules/shorthand-property.js',
+        './rules/string.js',
+        './rules/time.js',
+        './rules/unit.js',
+        './rules/value.js',
+        './rules/value-list.js'
+    ].map(require.resolve)
+};
