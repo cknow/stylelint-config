14e1e75f71e19544f7f0026544958df84ba44d16
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2017-11-28 09:07:18 -0200

    feat(rules): Adde `font-family-no-missing-generic-family-keyword` rule

--- rules/font-family.js
+++ rules/font-family.js
@@ -3,6 +3,7 @@
 module.exports = {
     rules: {
         'font-family-name-quotes': 'always-where-recommended',
-        'font-family-no-duplicate-names': true
+        'font-family-no-duplicate-names': true,
+        'font-family-no-missing-generic-family-keyword': true
     }
 };
