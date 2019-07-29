9295ec659af943744d9ac696f9bd37476a93b723
Ricardo Gobbo de Souza <ricardo@datalogix.com.br>
2019-07-29 16:53:12 -0300

    feat(scss): add rule `selector-nest-combinators`

--- rules/scss/selector.js
+++ rules/scss/selector.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     rules: {
+        'scss/selector-nest-combinators': 'always',
         'scss/selector-no-redundant-nesting-selector': true,
         'scss/selector-no-union-class-name': true
     }
