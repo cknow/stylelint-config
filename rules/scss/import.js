bbd9e4645714796131f6be5fca0fa28adbda08f4
Ricardo Gobbo de Souza <ricardo@datalogix.com.br>
2019-09-13 17:55:57 -0300

    feat(scss): add rule `scss/at-import-partial-extension`

--- rules/scss/import.js
+++ rules/scss/import.js
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
     'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-import-partial-extension': 'never',
     'scss/at-import-partial-extension-blacklist': null,
     'scss/at-import-partial-extension-whitelist': null
   }
